6d4e8b21756831c13d21818b9af14e79e3fcdcb1
Ji-Yoon Lee <dlwldbs369@tukorea.ac.kr>
2024-03-16 16:31:17 +0900

    init project

---
+++ teamAnt/src/main/java/com/backend/teamant/TeamAntApplication.java
@@ -0,0 +1,13 @@
+package com.backend.teamant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamAntApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamAntApplication.class, args);
+    }
+
+}
